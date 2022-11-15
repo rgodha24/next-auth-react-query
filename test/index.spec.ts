2be2d8c89bed9bdc159a346de978b85a62beaa7a
Rohan Godha <rgodha24@brophybroncos.org>
2022-11-15 13:48:12 -0700

    test(fix test): fixed test by writing one

---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+describe("index", () => {
+   describe("myPackage", () => {
+      it("should test something (im too lazy to write it tho)", () => {
+         expect("").toMatch("");
+      });
+   });
+});
