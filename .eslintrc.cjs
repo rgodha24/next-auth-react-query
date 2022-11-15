f8671d07fe60832ecea095f08636de91984cb365
Rohan Godha <rgodha24@brophybroncos.org>
2022-11-15 13:13:26 -0700

    feat(usesession): tanstack react query based useSession
    tanstack react query based useSession
    
    BREAKING CHANGE: tanstack react query based useSession

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+   root: true,
+   parser: "@typescript-eslint/parser",
+   plugins: ["@typescript-eslint", "node", "prettier"],
+   parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ["./tsconfig.json"],
+   },
+   extends: [
+      "eslint:recommended",
+      "plugin:node/recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "plugin:prettier/recommended",
+   ],
+   rules: {
+      "prettier/prettier": "off",
+      "node/no-missing-import": "off",
+      "node/no-empty-function": "off",
+      "node/no-unsupported-features/es-syntax": "off",
+      "node/no-missing-require": "off",
+      "node/shebang": "off",
+      "@typescript-eslint/no-use-before-define": 1,
+      "node/no-unpublished-import": "off",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-var-requires": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+   },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,62 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import parseUrl from "next-auth/utils/parse-url";
+import { fetchData } from "next-auth/client/_utils";
+import type { NextAuthClientConfig } from "next-auth/client/_utils";
+import type { GetSessionParams } from "next-auth/react";
+import type { Session } from "next-auth";
+import { proxyLogger } from "next-auth/utils/logger";
+import _logger from "next-auth/utils/logger";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+
+const __NEXTAUTH: NextAuthClientConfig = {
+   baseUrl: parseUrl(process.env.NEXTAUTH_URL ?? process.env.VERCEL_URL).origin,
+   basePath: parseUrl(process.env.NEXTAUTH_URL).path,
+   baseUrlServer: parseUrl(
+      process.env.NEXTAUTH_URL_INTERNAL ??
+         process.env.NEXTAUTH_URL ??
+         process.env.VERCEL_URL
+   ).origin,
+   basePathServer: parseUrl(
+      process.env.NEXTAUTH_URL_INTERNAL ?? process.env.NEXTAUTH_URL
+   ).path,
+   _lastSync: 0,
+   _session: undefined,
+   // eslint-disable-next-line @typescript-eslint/no-empty-function
+   _getSession: () => {},
+};
+const logger = proxyLogger(_logger, __NEXTAUTH.basePath);
+
+export const getSession = async (params?: GetSessionParams) =>
+   fetchData<Session>("session", __NEXTAUTH, logger, params);
+
+const useSession = (params?: UseSessionParams) => {
+   const query = useQuery(["useSession"], {
+      queryFn: () => getSession(),
+      ...params,
+   });
+
+   if (query.isSuccess) {
+      if (query.data === null)
+         return {
+            status: "unauthenticated" as const,
+            data: null,
+            reactQueryData: { ...query },
+         };
+      return {
+         status: "authenticated" as const,
+         data: query.data,
+         reactQueryData: { ...query },
+      };
+   }
+   return { status: "loading" as const, data: null, reactQueryData: { ...query } };
+};
+
+export type UseSessionParams = Omit<
+   UseQueryOptions<
+      Session | null | undefined,
+      unknown,
+      Session | null | undefined,
+      string[]
+   >,
+   "queryFn"
+>;
+export default useSession;
